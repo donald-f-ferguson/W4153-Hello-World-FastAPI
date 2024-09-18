43ba68db8481e4ffa33dea70d3d78798785e38cc
donald.ferguson <donff2@aol.com>
2024-09-18 05:16:34 -0400

    Added returning hello with a message.

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ async def root():
     return {"\n\nmessage": "Hello from W4153-Hello-World-FastAPI!\n\n"}
 
 
+@app.get("/hello/{name}")
+async def hello(name: str):
+    msg = f"Hello {name} from W4153-Hello-World-FastAPI!"
+    return {"\n\nmessage": msg}
+
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
